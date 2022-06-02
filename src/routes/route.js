9dabfb82aad2fe98a0d7aa8fe171470b076c8a33
Likemanu1999 <manishspatil345@gmail.com>
2022-06-02 14:55:40 +0530

    nodemodules assingment

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const externalModule = require('../logger/logger')
 const externalModule1 = require('../util/helper')
 const externalModule2 = require('../validator/formatter')
 
+
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
@@ -17,17 +18,7 @@ router.get('/test-me1', function (req, res) {
     res.send('My second ever api!')
 });
 
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
 
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
-
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
 
 module.exports = router;
 // adding this comment for no reason
